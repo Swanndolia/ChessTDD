ba1e71dabdb2049b86258bb740f64cf06255f2b3
Swanndolia <Swanndolia@gmail.com>
2024-07-04 13:02:57 +0200

    Test for gameBoard generation and correct pieces initialisation

--- src/test/java/chessboard/ChessBoardTest.java
+++ src/test/java/chessboard/ChessBoardTest.java
@@ -1,30 +1,64 @@
 package chessboard;
 
-import lombok.extern.java.Log;
 import net.swanndolia.gameboard.ChessBoard;
 import net.swanndolia.gameboard.Square;
+import net.swanndolia.pieces.*;
 import net.swanndolia.utils.Color;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
+import org.junit.jupiter.api.Test;
 
 import java.util.Random;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ChessBoardTest {
     ChessBoard chessBoard = new ChessBoard();
 
+    @BeforeEach
+    public void initChessboard() {
+        this.chessBoard.initChessboard();
+    }
+
     @RepeatedTest(50)
     public void gameboardShouldAlternateBlackAndWhiteCells() {
-        chessBoard.initChessboard();
-        Square square = chessBoard.getGameBoard()[new Random().nextInt(chessBoard.getGameBoardSize() - 1)][new Random().nextInt(
-                chessBoard.getGameBoardSize() - 1)];
+        Color squareColor;
+        Square square = this.chessBoard.getGameBoard()[new Random().nextInt(this.chessBoard.getGameBoardSize() - 1)][new Random().nextInt(
+                this.chessBoard.getGameBoardSize() - 1)];
         if (square.getHorizontalCoordinates() % 2 == 0 && square.getVerticalCoordinates() % 2 == 0) {
-            assertThat(square.getColor().equals(Color.WHITE));
+            squareColor = Color.WHITE;
         } else if (square.getHorizontalCoordinates() % 2 != 0 && square.getVerticalCoordinates() % 2 != 0) {
-            assertThat(square.getColor().equals(Color.WHITE));
+            squareColor = Color.WHITE;
         } else {
-            assertThat(square.getColor().equals(Color.BLACK));
+            squareColor = Color.BLACK;
+        }
+        assertThat(square.getColor()).isEqualTo(squareColor);
+    }
+
+    @Test
+    public void gameBoardShouldBeFilledWithAllStartingPiecesAtCorrectPosition() {
+        Piece piece = null;
+        Color color = Color.WHITE;
+        for (int vertical = 0; vertical < this.chessBoard.getGameBoardSize(); vertical++) {
+            if (vertical == 6) {
+                color = Color.BLACK;
+            }
+            for (int horizontal = 0; horizontal < chessBoard.getGameBoardSize(); horizontal++) {
+                if (vertical == 0 || vertical == 7) {
+                    switch (horizontal) {
+                        case 0, 7 -> piece = new Rook(color, this.chessBoard.getSquare(vertical, horizontal));
+                        case 1, 6 -> piece = new Knight(color, this.chessBoard.getSquare(vertical, horizontal));
+                        case 2, 5 -> piece = new Bishop(color, this.chessBoard.getSquare(vertical, horizontal));
+                        case 3 -> piece = new King(color, this.chessBoard.getSquare(vertical, horizontal));
+                        case 4 -> piece = new Queen(color, this.chessBoard.getSquare(vertical, horizontal));
+                    }
+                } else if (vertical == 1 || vertical == 6) {
+                    piece = new Pawn(color, this.chessBoard.getSquare(vertical, horizontal));
+                } else {
+                    piece = null;
+                }
+                assertThat(piece).isEqualTo(this.chessBoard.getSquare(vertical, horizontal).getPiece());
+            }
         }
     }
 }
