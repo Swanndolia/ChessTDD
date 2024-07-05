a47a6c0689194710c0ba17aa5175670acb38735c
Swanndolia <Swanndolia@gmail.com>
2024-07-05 14:15:53 +0200

    test to get next square on the board in a direction form white and black perspective

--- src/test/java/chessboard/SquareIsAttackedTest.java
+++ src/test/java/chessboard/SquareIsAttackedTest.java
@@ -1,11 +1,12 @@
 package chessboard;
 
 import net.swanndolia.gameboard.ChessBoard;
+import net.swanndolia.gameboard.Square;
+import net.swanndolia.moves.MoveDirection;
 import net.swanndolia.utils.Color;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -22,5 +23,43 @@ public class SquareIsAttackedTest {
     @EnumSource(Color.class)
     public void givenTheGameJustStartedWhiteAndBlackShouldBothHave16Pieces(Color color) {
         assertThat(color == Color.WHITE ? chessBoard.getWhitePieces().size() : chessBoard.getBlackPieces().size()).isEqualTo(16);
+    }
+
+    @ParameterizedTest
+    @EnumSource(MoveDirection.class)
+    public void givenASquareOnAChessBoardIShouldBeAbleToGetTheNextSquareInAnyDirectionFromWhitePerspective(MoveDirection moveDirection) {
+        Square square          = chessBoard.getSquare(4, 4);
+        Square nextSquare      = chessBoard.getNextSquare(square, moveDirection, Color.WHITE);
+        Square expectedSquared = square;
+        switch (moveDirection) {
+            case LEFT -> expectedSquared = chessBoard.getSquare(4, 5);
+            case RIGHT -> expectedSquared = chessBoard.getSquare(4, 3);
+            case DIAGONAL_UP_LEFT -> expectedSquared = chessBoard.getSquare(5, 5);
+            case DIAGONAL_UP_RIGHT -> expectedSquared = chessBoard.getSquare(5, 3);
+            case DIAGONAL_DOWN_LEFT -> expectedSquared = chessBoard.getSquare(3, 5);
+            case DIAGONAL_DOWN_RIGHT -> expectedSquared = chessBoard.getSquare(3, 3);
+            case FORWARD -> expectedSquared = chessBoard.getSquare(5, 4);
+            case BACKWARD -> expectedSquared = chessBoard.getSquare(3, 4);
+        }
+        assertThat(nextSquare).isEqualTo(expectedSquared);
+    }
+
+    @ParameterizedTest
+    @EnumSource(MoveDirection.class)
+    public void givenASquareOnAChessBoardIShouldBeAbleToGetTheNextSquareInAnyDirectionFromBlackPerspective(MoveDirection moveDirection) {
+        Square square          = chessBoard.getSquare(4, 4);
+        Square nextSquare      = chessBoard.getNextSquare(square, moveDirection, Color.BLACK);
+        Square expectedSquared = square;
+        switch (moveDirection) {
+            case LEFT -> expectedSquared = chessBoard.getSquare(4, 3);
+            case RIGHT -> expectedSquared = chessBoard.getSquare(4, 5);
+            case DIAGONAL_UP_LEFT -> expectedSquared = chessBoard.getSquare(3, 3);
+            case DIAGONAL_UP_RIGHT -> expectedSquared = chessBoard.getSquare(3, 5);
+            case DIAGONAL_DOWN_LEFT -> expectedSquared = chessBoard.getSquare(5, 3);
+            case DIAGONAL_DOWN_RIGHT -> expectedSquared = chessBoard.getSquare(5, 5);
+            case FORWARD -> expectedSquared = chessBoard.getSquare(3, 4);
+            case BACKWARD -> expectedSquared = chessBoard.getSquare(5, 4);
+        }
+        assertThat(nextSquare).isEqualTo(expectedSquared);
     }
 }
